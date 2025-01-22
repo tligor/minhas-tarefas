3957b11ce5167b0500522b8df4c09de470842d72
tligor <santanna.ig@hotmail.com>
2025-01-22 17:28:32 -0300

    Modificado styles para filtro de seleção de tarefas, adicionado estilo a barra de pesquisa

--- src/Container/BarraLateral/index.tsx
+++ src/Container/BarraLateral/index.tsx
@@ -1,3 +1,19 @@
-const BarraLateral = () => <aside>barra lateral</aside>
+import FiltroCard from '../../Components/FiltroCard'
+import * as S from './styles'
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Buscar..."></S.Campo>
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard ativo />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
