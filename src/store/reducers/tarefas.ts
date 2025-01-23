c8cb2f722072166748f4da4fd4b8226c32291b1d
tligor <santanna.ig@hotmail.com>
2025-01-23 04:05:19 -0300

    As taregas estao sendo gerenciadas por uma função, reduzir chamadas

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -37,9 +37,17 @@ const tarefasSlice = createSlice({
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
       state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
