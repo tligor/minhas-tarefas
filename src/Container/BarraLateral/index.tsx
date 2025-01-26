43bc898fa029c4406544b97a16b853a07166e6b6
tligor <santanna.ig@hotmail.com>
2025-01-25 21:32:39 -0300

    adicionado busca por tarefas, e resolvido problema com letras maiúsculas e minusculas com toLowerCase na busca e no termo.

--- src/Container/BarraLateral/index.tsx
+++ src/Container/BarraLateral/index.tsx
@@ -1,19 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../Components/FiltroCard'
 import * as S from './styles'
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="Buscar..."></S.Campo>
-      <S.Filtros>
-        <FiltroCard legendas="pendente" contador={1} />
-        <FiltroCard legendas="concluída" contador={2} ativo />
-        <FiltroCard legendas="urgentes" contador={2} />
-        <FiltroCard legendas="importantes" contador={4} />
-        <FiltroCard legendas="normal" contador={5} />
-        <FiltroCard legendas="todas" contador={14} />
-      </S.Filtros>
-    </div>
-  </S.Aside>
-)
+import { RootReducer } from '../../store'
+import { alterarTermo } from '../../store/reducers/filtro'
+
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="Buscar..."
+          value={termo}
+          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+        ></S.Campo>
+        <S.Filtros>
+          <FiltroCard legendas="pendente" contador={1} />
+          <FiltroCard legendas="concluída" contador={2} ativo />
+          <FiltroCard legendas="urgentes" contador={2} />
+          <FiltroCard legendas="importantes" contador={4} />
+          <FiltroCard legendas="normal" contador={5} />
+          <FiltroCard legendas="todas" contador={14} />
+        </S.Filtros>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
---
+++ src/store/reducers/filtro.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import * as enums from '../../utils/enums/tarefa'
+import filtroReducer from './filtro'
+
+type filtroState = {
+  termo: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
+}
+
+const initialState: filtroState = {
+  termo: '',
+  criterio: 'todas'
+}
+
+const filtroSlice = createSlice({
+  name: 'filtro',
+  initialState,
+  reducers: {
+    alterarTermo: (state, action: PayloadAction<string>) => {
+      state.termo = action.payload
+    }
+  }
+})
+
+export const { alterarTermo } = filtroSlice.actions
+
+export default filtroSlice.reducer
